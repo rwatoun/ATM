f65f4f6aeb22f21d68da83d3a1f5cce016fbef87
Marwa Hammani <hammanimarwa0@gmail.com>
2024-12-15 13:06:39 -0500

    created the display options and gave them loops and try and catches. also resolved some errors

--- ATM/Program.cs
+++ ATM/Program.cs
@@ -14,7 +14,7 @@ public class cardHolder
         this.pin = pin;
         this.firstName = firstName;
         this.lastName = lastName;
-        double cardBalance = cardBalance;
+        this.cardBalance = cardBalance;
 
     }
 
@@ -40,7 +40,7 @@ public class cardHolder
         return cardBalance;
     }
 
-    public void setCardNum(int newCardNum)
+    public void setCardNum(string newCardNum)
     {
         this.cardNum = newCardNum;
     }
@@ -94,13 +94,112 @@ public class cardHolder
             else
             {
                 currentUser.setCardBalance(currentUser.getCardBalance() - withdrawal);
-                ConsoleWriteLine("You're good to go! Thank you.");
+                Console.WriteLine("You're good to go! Thank you.");
             }
         }
 
         void balance(cardHolder currentUser)
         {
-            Console.WriteLine("Current Balance: " + currentUser.getCardBalance())
+            Console.WriteLine("Current Balance: " + currentUser.getCardBalance());
         }
+
+        List<cardHolder> cardHolders = new List<cardHolder>();
+        cardHolders.Add(new cardHolder("45327772818527395", 1234, "John", "Doe", 150.31));
+        cardHolders.Add(new cardHolder("45327672881527392", 3210, "Ashley", "Jones", 321.13));
+        cardHolders.Add(new cardHolder("51283322818527395", 9999, "Antony", "Dickerson", 105.59));
+        cardHolders.Add(new cardHolder("60117772818527215", 2468, "Muneb", "Harding", 851.84));
+        cardHolders.Add(new cardHolder("34907772818521495", 4826, "Dawn", "Smith", 54.27));
+
+        // Prompting the user!
+        Console.WriteLine("Welcome to SimpleATM");
+        Console.WriteLine("Please insert your debit card: ");
+        string debitCardNum = "";
+        cardHolder currentUser;
+
+        while (true)
+        {
+            try
+            {
+                debitCardNum = Console.ReadLine();
+                //Check against DB
+                currentUser = cardHolders.FirstOrDefault(a => a.cardNum == debitCardNum);
+                if (currentUser != null)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Card not recognized. Please try again.");
+                }
+
+
+            }
+            catch
+            {
+                Console.WriteLine("Card not recognized. Please try again.");
+
+            }
+
+        }
+
+        Console.WriteLine("Please enter your pin: ");
+        int userPin = 0;
+        while (true)
+        {
+            try
+            {
+                userPin = int.Parse(Console.ReadLine());
+                if (currentUser.getPin() == userPin)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Pin not recognized. Please try again.");
+                }
+
+
+            }
+            catch
+            {
+                Console.WriteLine("Pin not recognized. Please try again.");
+
+            }
+
+        }
+        Console.WriteLine("Welcome " + currentUser.getFirstName() + " !");
+        int option = 0;
+        do
+        {
+            printOptions();
+            try
+            {
+                option = int.Parse(Console.ReadLine());
+            }
+            catch { }
+
+            switch (option)
+            {
+                case 1:
+                    deposit(currentUser);
+                    option = 0;
+                    break;
+                case 2:
+                    withdraw(currentUser);
+                    option = 0;
+                    break;
+                case 3:
+                    balance(currentUser);
+                    option = 0;
+                    break;
+                case 4:
+                    break;
+
+            }
+
+        }
+        while (option != 4);
+        Console.WriteLine("Thank you! Have a nice day :)");
+
     }
 }
